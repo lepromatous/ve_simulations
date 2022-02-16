afb9f58cdd6b9e6dd8c93399150e1706b0e6ba37
Timothy Wiemken <tim.wiemken@gmail.com>
2022-02-15 20:06:43 -0600

    added 30% NEJM and 40% jama

--- VE simulations.R
+++ VE simulations.R
@@ -71,12 +71,47 @@ mod1.pois.adj <- poisson.rev(mod1.pois)
 
 
 
+############################################################################################
+##### 30% like this: https://www.nejm.org/doi/pdf/10.1056/NEJMoa2106599
+## VE = 63%
+1-((167/1472) / (1072/3420)) ### 
+## 63%
+
+(1472)/(3420+1472) # ~ 30% prevalence  
+
+covid <- c(rep(1, times=1472), rep(0, times=3420))
+vaccine.status <- c(rep(1, times=167), rep(0, times=1472-167), rep(1, times=1072), rep(0, times=3420-1072))
+
+### with logistic
+mod1 <- glm(covid ~ vaccine.status, family="binomial")
+1-exp(mod1$coefficients) # ~72% ve
+
+#### with poisson
+poisson.rev<-function(a){
+  require(sandwich) || install.packages(sandwich) 
+  cov.m1 <- vcovHC(a, type = "HC0")
+  std.err <- sqrt(diag(cov.m1))
+  r.est <- cbind(Estimate = coef(a), `Robust SE` = std.err, `Pr(>|z|)` = 2 * 
+                   pnorm(abs(coef(a)/std.err), lower.tail = FALSE), LL = coef(a) - 1.96 * 
+                   std.err, UL = coef(a) + 1.96 * std.err)
+  RR.CI<-exp(r.est[,c(1,4,5)])
+  output<-cbind(RR.CI, r.est[,3])
+  colnames(output)<-c("Risk Ratio", "Lower 95% CI", "Upper 95% CI", "P-value")
+  output<-round(output,3)
+  return(output)
+}
+mod1.pois <- mod1 <- glm(covid ~ vaccine.status, family="poisson")
+mod1.pois.adj <- poisson.rev(mod1.pois)
+1 - mod1.pois.adj[2,1] ## 62.3% ve - odds overestimates 10 absolute percentage points
+
+
 
 
 
 
 ###########################################################################################
 ##### what about 40% prevalence of covid?
+### example: https://jamanetwork.com/journals/jama/fullarticle/2786039
 ## VE = 71%
 1-((90/1000) / (310/1000)) ### 90 cases vaccine group, 310 cases non vaccine group
 ## 71%
